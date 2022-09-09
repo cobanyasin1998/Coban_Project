c7097526e7a1ea200ed6c07c2fecebec41c404f8
Yasin Çoban <yasincoban1998@outlook.com>
2022-09-09 19:40:26 +0300

    categoryApı controller added

--- Coban/Coban.API/Controllers/CategoryController.cs
+++ Coban/Coban.API/Controllers/CategoryController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Coban.Core.Services;
+using Coban.DTO;
+using Coban.Entities.Entity;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Coban.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class CategoryController : ControllerBase
+    public class CategoryController : CustomBaseController
     {
 
+        private IServiceGeneric<Category, CategoryDTO> _catService;
+        public CategoryController(IServiceGeneric<Category, CategoryDTO> catService)
+        {
+            _catService = catService;
+        }
 
+        [HttpGet]
+        public async Task<IActionResult> GetProducts()
+        {
+            return ActionResultInstance(await _catService.GetAllAsync());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SaveProducts(ProductDTO productDTO)
+        {
+            return ActionResultInstance(await _catService.AddAsync(productDTO));
+        }
+        [HttpPut]
+        public async Task<IActionResult> UpdateProduct(ProductDTO productDTO)
+        {
+            return ActionResultInstance(await _catService.UpdateAsync(productDTO, productDTO.Id));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProduct(int id)
+        {
+            return ActionResultInstance(await _catService.Remove(id));
+        }
     }
 }
---
+++ Coban/Coban.API/Controllers/CustomBaseController.cs
@@ -0,0 +1,21 @@
+﻿using Coban.SharedLibrary.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Coban.API.Controllers
+{
+    public class CustomBaseController : ControllerBase
+    {
+        public IActionResult ActionResultInstance<T>(ResponseDTO<T> response) where T : class
+        {
+            return new ObjectResult(response)
+            {
+                StatusCode = response.Status
+            };
+        }
+    }
+}
