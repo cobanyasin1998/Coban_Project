8bbb96d78cb931f84a305801b951225cb078a94e
Yasin Çoban <yasincoban1998@outlook.com>
2022-09-11 19:46:30 +0300

    cobanlib.js added

---
+++ Coban/Coban.UI/wwwroot/js/CobanLib.js
@@ -0,0 +1,14 @@
+﻿var Lib = {
+    SweetAlertNotification: function (icon,err,time = 1500) {
+        Swal.fire({
+            position: 'top-end',
+            icon: icon,
+            title: err,
+            showConfirmButton: false,
+            timer: time
+        });
+    }
+};
+
+
+
